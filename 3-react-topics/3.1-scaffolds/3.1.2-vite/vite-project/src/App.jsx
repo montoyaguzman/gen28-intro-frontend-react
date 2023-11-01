b8dea451d80e50913f1e06853b1fe9f46ac77887
montoyaguzman7 <montoyaguzman7@gmail.com>
2023-10-31 21:15:15 -0600

    feat: props enm react

--- 3-react-topics/3.1-scaffolds/3.1.2-vite/vite-project/src/App.jsx
+++ 3-react-topics/3.1-scaffolds/3.1.2-vite/vite-project/src/App.jsx
@@ -3,16 +3,23 @@ import React from "react";
 import { Card } from "./components/Card";
 import { Header } from "./components/Header";
 
+import bulbiImg from './assets/img/bulbi.jpg';
+import squirtleImg from './assets/img/squirtle.jpg';
+import charmanderImg from './assets/img/charmander.png';
+
 function App() {
+
+  const bulbasaur = { id: 1, name: 'Bulbasaur', img: bulbiImg };
+  const squirtle = { id: 2, name: 'Squirtle', img: squirtleImg };
+  const charmander = { id: 3, name: 'Charmander', img: charmanderImg };
 
   return ( /** JSX */
     <React.Fragment>
       <Header />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {/* <input type="text" placeholder="escribe tu usuario..." value="montoya" /> */}
+      <Card name={bulbasaur.name} img={bulbasaur.img} />
+      <Card name={squirtle.name} img={squirtle.img} />
+      <Card name={charmander.name} img={charmander.img} />
     </React.Fragment>
   )
 }
--- 3-react-topics/3.1-scaffolds/3.1.2-vite/vite-project/src/components/Card/index.jsx
+++ 3-react-topics/3.1-scaffolds/3.1.2-vite/vite-project/src/components/Card/index.jsx
@@ -1,16 +1,17 @@
 
 import React from "react";
-import pokemon from '../../assets/img/bulbi.jpg';
 import './styles.css';
 
-function Card() {
+function Card(props) {
+
+  console.log('props', props)
 
   return ( /** JSX */
     <React.Fragment>
       <div className="card">
-        <h2 className="title-card">Bulbasaur</h2>
-        <img src={pokemon} alt="Este es un pokemon" width="200" height="200" />
-        <button data-index="1" className="button">saluda</button>
+        <h2 className="title-card">{props.name}</h2>
+        <img src={props.img} alt="Este es un pokemon" width="200" height="200" />
+        <button data-index="1" className="button">{props.name}</button>
       </div>
     </React.Fragment>
   )
