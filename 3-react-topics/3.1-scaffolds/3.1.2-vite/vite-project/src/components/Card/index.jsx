e3510a00f9cff04f3cac6e34003c4b3dbfe6a923
montoyaguzman7 <montoyaguzman7@gmail.com>
2023-10-31 20:34:29 -0600

    feat: inserta imgs y estilos a los componentes

--- 3-react-topics/3.1-scaffolds/3.1.2-vite/vite-project/src/components/Card/index.jsx
+++ 3-react-topics/3.1-scaffolds/3.1.2-vite/vite-project/src/components/Card/index.jsx
@@ -1,14 +1,17 @@
 
 import React from "react";
+import pokemon from '../../assets/img/bulbi.jpg';
+import './styles.css';
 
 function Card() {
 
-  const nameComponent = 'patito';
-
   return ( /** JSX */
     <React.Fragment>
-      <h1>{nameComponent} component works!</h1>
-      <span>aqui iria el html de la tarjetita</span>
+      <div className="card">
+        <h2 className="title-card">Bulbasaur</h2>
+        <img src={pokemon} alt="Este es un pokemon" width="200" height="200" />
+        <button data-index="1" className="button">saluda</button>
+      </div>
     </React.Fragment>
   )
 }
