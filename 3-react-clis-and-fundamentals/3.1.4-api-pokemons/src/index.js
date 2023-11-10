126b008cc98f4f6c5779ae0e894953f4f24cae86
montoyaguzman7 <montoyaguzman7@gmail.com>
2023-11-09 19:44:33 -0600

    feat: agrega cors al api de express

--- 3-react-clis-and-fundamentals/3.1.4-api-pokemons/src/index.js
+++ 3-react-clis-and-fundamentals/3.1.4-api-pokemons/src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 3000;
 const { pokemons } = require('../data/pokemons');
+
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Servidor pokemon runging in ${port}`);
