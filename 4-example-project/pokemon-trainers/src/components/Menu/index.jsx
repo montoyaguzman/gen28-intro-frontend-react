28d51169efa699241a2649d2a7e6ee5f92ab73a3
montoyaguzman7 <montoyaguzman7@gmail.com>
2023-11-14 20:09:30 -0600

    feat: agrega estilos para el menu

--- 4-example-project/pokemon-trainers/src/components/Menu/index.jsx
+++ 4-example-project/pokemon-trainers/src/components/Menu/index.jsx
@@ -4,21 +4,28 @@ import {
 } from 'react-router-dom';
 import './styles.css';
 
+import logo from '../../assets/img/logo.png';
+
 function Menu({ children }) {
     // const children = props.children
     return (
         <>
             <Router>
                 <nav style={{ margin: 10 }}>
-                    <Link to="/" style={{ padding: 5 }}>
-                        Inicio
-                    </Link>
-                    <Link to="/new-trainer" style={{ padding: 5 }}>
-                        Crear un nuevo entrenador
-                    </Link>
-                    <Link to="/about" style={{ padding: 5 }}>
-                        Acerca de nosotros
-                    </Link>
+                    <div>
+                        <img src={logo} alt="logo de trainers" className='logo-img' />
+                    </div>
+                    <ul className='nav-items'>
+                        <Link to="/" className='nav-link'>
+                            Inicio
+                        </Link>
+                        <Link to="/new-trainer" className='nav-link'>
+                            Crear un nuevo entrenador
+                        </Link>
+                        <Link to="/about" className='nav-link'>
+                            Acerca de nosotros
+                        </Link>
+                    </ul>
                 </nav>
 
                 {/* Outlet: Es la pagina que se va a mostrar */}
